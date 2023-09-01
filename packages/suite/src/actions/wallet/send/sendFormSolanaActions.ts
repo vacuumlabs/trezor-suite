b67e59b60efa3161b0c5620e5e493f3649d27c7d
Samuel Šulovský <samuel.sulovsky@gmail.com>
2023-09-01 13:49:30 +0200

    feat(suite): calculate normal SOL fees

--- packages/suite/src/actions/wallet/send/sendFormSolanaActions.ts
+++ packages/suite/src/actions/wallet/send/sendFormSolanaActions.ts
@@ -1,16 +1,24 @@
-import TrezorConnect from '@trezor/connect';
+import TrezorConnect, { FeeLevel, TokenInfo } from '@trezor/connect';
 import { Transaction, SystemProgram, PublicKey } from '@solana/web3.js';
 import {
     FormState,
     PrecomposedTransactionFinal,
     ComposeActionContext,
+    ExternalOutput,
+    PrecomposedTransaction,
+    PrecomposedLevels,
 } from '@suite-common/wallet-types';
 import { notificationsActions } from '@suite-common/toast-notifications';
 import { Dispatch, GetState } from 'src/types/suite';
-import { getExternalComposeOutput, getLamportsFromSol } from '@suite-common/wallet-utils';
+import {
+    calculateMax,
+    calculateSolFee,
+    calculateTotal,
+    formatAmount,
+    getExternalComposeOutput,
+    getLamportsFromSol,
+} from '@suite-common/wallet-utils';
 import BigNumber from 'bignumber.js';
-
-const FEE_PER_SIGNATURE = new BigNumber(500);
 
 // TODO(vl): Implement send-max
 // TODO(vl), phase 2: Implement custom Prioritization Fees
@@ -34,43 +42,122 @@ const getRecentBlockhash = async () => {
     return result.value.blockhash;
 };
 
+// TODO(vl): Handle token transfers -- see ETH implementation
+const calculate = (
+    availableBalance: string,
+    output: ExternalOutput,
+    feeLevel: FeeLevel,
+    token?: TokenInfo,
+): PrecomposedTransaction => {
+    // TODO(vl): handle prioritisation fee
+    // 1, 0, 0 = discard prioritisation fee for now
+    // Default args include the prioritisation fee
+    const feeInSatoshi = calculateSolFee(1, 0, 0);
+    let amount: string;
+    let max: string | undefined;
+    if (output.type === 'send-max' || output.type === 'send-max-noaddress') {
+        max = calculateMax(availableBalance, feeInSatoshi);
+        amount = max;
+    } else {
+        amount = output.amount;
+    }
+
+    // total SOL spent (amount + fee), in case of SPL token only the fee
+    const totalSpent = new BigNumber(calculateTotal(token ? '0' : amount, feeInSatoshi));
+
+    if (totalSpent.isGreaterThan(availableBalance)) {
+        const error = token ? 'AMOUNT_NOT_ENOUGH_CURRENCY_FEE' : 'AMOUNT_IS_NOT_ENOUGH';
+        // errorMessage declared later
+        return { type: 'error', error, errorMessage: { id: error } } as const;
+    }
+
+    const payloadData = {
+        type: 'nonfinal',
+        totalSpent: token ? amount : totalSpent.toString(),
+        max,
+        fee: feeInSatoshi,
+        feePerByte: feeLevel.feePerUnit,
+        feeLimit: feeLevel.feeLimit,
+        token,
+        bytes: 0,
+    } as const;
+
+    if (output.type === 'send-max' || output.type === 'external') {
+        return {
+            ...payloadData,
+            type: 'final',
+            // compatibility with BTC PrecomposedTransaction from @trezor/connect
+            transaction: {
+                inputs: [],
+                outputsPermutation: [0],
+                outputs: [
+                    {
+                        address: output.address,
+                        amount,
+                        script_type: 'PAYTOADDRESS',
+                    },
+                ],
+            },
+        };
+    }
+
+    return payloadData;
+};
+
 export const composeTransaction =
     (formValues: FormState, formState: ComposeActionContext) => () => {
-        const { account, network } = formState;
+        const { account, network, feeInfo } = formState;
         const composeOutputs = getExternalComposeOutput(formValues, account, network);
         if (!composeOutputs) return; // no valid Output
 
-        const { address, amount } = formValues.outputs[0];
+        const { output, decimals } = composeOutputs;
 
-        // TODO(vl), phase 2: account for token transfers -- should only count fee when transferring tokens
-        const totalSpent = new BigNumber(amount)
-            .times(new BigNumber(10 ** 9))
-            .plus(FEE_PER_SIGNATURE)
-            .toString();
+        // TODO(vl): get custom fee limit from TrezorConnect.BlockchainEstimateFee
+        const customFeeLimit: string | undefined = undefined; // FEE_PER_SIGNATURE.toString();
 
-        const feeObject = {
-            normal: {
-                type: 'final',
-                totalSpent,
-                fee: '500',
-                feePerByte: '500',
-                feeLimit: '500',
-                bytes: 0,
-                transaction: {
-                    inputs: [],
-                    outputsPermutation: [0],
-                    outputs: [
-                        {
-                            address,
-                            amount: new BigNumber(amount).times(new BigNumber(10 ** 9)).toString(),
-                            script_type: 'PAYTOADDRESS',
-                        },
-                    ],
-                },
-                estimatedFeeLimit: '500',
-            },
-        };
-        return Promise.resolve(feeObject);
+        // FeeLevels are read-only
+        const levels = customFeeLimit ? feeInfo.levels.map(l => ({ ...l })) : feeInfo.levels;
+        const predefinedLevels = levels.filter(l => l.label !== 'custom');
+        // update predefined levels with customFeeLimit (gasLimit from data size or erc20 transfer)
+        if (customFeeLimit) {
+            predefinedLevels.forEach(l => (l.feeLimit = customFeeLimit));
+        }
+        // in case when selectedFee is set to 'custom' construct this FeeLevel from values
+        if (formValues.selectedFee === 'custom') {
+            predefinedLevels.push({
+                label: 'custom',
+                feePerUnit: formValues.feePerUnit,
+                feeLimit: formValues.feeLimit,
+                blocks: -1,
+            });
+        }
+
+        const wrappedResponse: PrecomposedLevels = {};
+        const response = predefinedLevels.map(level =>
+            calculate(account.availableBalance, output, level),
+        );
+        response.forEach((tx, index) => {
+            const feeLabel = predefinedLevels[index].label as FeeLevel['label'];
+            wrappedResponse[feeLabel] = tx;
+        });
+
+        // format max (calculate sends it as satoshi)
+        // update errorMessage values (symbol)
+        Object.keys(wrappedResponse).forEach(key => {
+            const tx = wrappedResponse[key];
+            if (tx.type !== 'error') {
+                tx.max = tx.max ? formatAmount(tx.max, decimals) : undefined;
+                tx.estimatedFeeLimit = customFeeLimit;
+            }
+            if (tx.type === 'error' && tx.error === 'AMOUNT_NOT_ENOUGH_CURRENCY_FEE') {
+                tx.errorMessage = {
+                    id: 'AMOUNT_NOT_ENOUGH_CURRENCY_FEE',
+                    values: { symbol: network.symbol.toUpperCase() },
+                };
+            }
+        });
+
+        return Promise.resolve(wrappedResponse);
     };
 
 export const signTransaction =
